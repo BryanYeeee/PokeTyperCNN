32d43fa520ca4e9b7fc8493dbcf2d9a4d588f4e0
BryanYeeee <haloday873@gmail.com>
2025-09-19 00:20:13 -0400

    home page augmented css

---
+++ poke-typer-frontend/src/app/components/dexCircles.js
@@ -0,0 +1,22 @@
+const DexCircles = () => {
+  return (
+    <svg
+      viewBox="0 0 620 210"
+      width="auto"
+      height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      role="img"
+    >
+      <circle cx="325" cy="40" r="25" fill="#b71c1c" stroke="#6b0000" strokeWidth="8" />
+      <circle cx="400" cy="40" r="25" fill="#f1c40f" stroke="#8b6b00" strokeWidth="8" />
+      <circle cx="475" cy="40" r="25" fill="#27ae60" stroke="#127032" strokeWidth="8" />
+
+      <circle cx="105" cy="105" r="100" fill="none" stroke="#1a1a1a" strokeWidth="5" opacity="1" />
+      <circle cx="105" cy="105" r="88" fill="none" stroke="#e6e6e6" strokeWidth="16" opacity="0.9" />
+      <circle cx="105" cy="105" r="80" fill="#4da6ff" stroke="#003366" strokeWidth="5" />
+      <circle cx="85" cy="85" r="20" fill="#b3e0ff" stroke="#004080" strokeWidth="1" opacity="0.4"/>
+    </svg>
+  )
+}
+
+export default DexCircles
--- poke-typer-frontend/src/app/page.js
+++ poke-typer-frontend/src/app/page.js
@@ -1,103 +1,53 @@
-import Image from "next/image";
+import Image from 'next/image'
+import DexCircles from './components/dexCircles'
 
-export default function Home() {
+export default function Home () {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div
+      id='bg-svg'
+      className='h-screen grid grid-cols-2 py-12 px-48 gap-4 items-end'
+    >
+      <div className='h-full relative'>
+        <div //calc(15%+4rem+10px)
+          className='h-[calc(15%+4rem-5px)] w-full foreground absolute p-8 flex items-center'
+          data-augmented-ui='tl-clip tr-clip both'
+          style={{
+            '--aug-inlay-bg': '#b21f1db8'
+          }}
+        >
+          <DexCircles />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className='h-[calc(85%+5px)] w-full absolute bottom-0'>
+          <div
+            className='size-full foreground  flex justify-center items-end'
+            data-augmented-ui='bl-clip br-clip  tl-clip-x'
+            style={{
+              '--aug-tl1': '4rem',
+              '--aug-tl-inset1': '50%'
+            }}
+          >
+            <div
+              className='h-[calc(100%-5px)] w-full foreground'
+              data-augmented-ui='bl-clip br-clip  tl-clip-x both'
+              style={{
+                '--aug-tl1': '4rem',
+                '--aug-tl-inset1': '50.5%'
+              }}
+            >
+              2
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className='h-17/20 w-full foreground'
+        data-augmented-ui='bl-clip br-clip tr-clip-x both'
+        style={{
+          '--aug-tr1': '4rem',
+          '--aug-tr-inset2': '50%'
+        }}
+      >
+      </div>
     </div>
-  );
+  )
 }
